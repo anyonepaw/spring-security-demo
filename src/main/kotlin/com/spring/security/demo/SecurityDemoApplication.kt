32d227705211bfb1961ac5e0ab5d0e08df9dd675
Anzhelika Pokhodun <anzhelika.pohodun@leroymerlin.ru>
2020-04-23 09:57:07 +0300

    Change the package's name

--- src/main/kotlin/com/spring/security/demo/SecurityDemoApplication.kt
+++ src/main/kotlin/com/spring/security/demo/SecurityDemoApplication.kt
@@ -1,11 +1,12 @@
-package com.spring.securitydemo
+package com.spring.security.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class SecurityDemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<SecurityDemoApplication>(*args)
+    runApplication<SecurityDemoApplication>(*args)
 }
